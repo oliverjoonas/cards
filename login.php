f7279d3833a415c31918fb2300ab55a122c91fcd
nutopaa5 <neuvonen.joonas@gmail.com>
2019-09-04 10:34:52 +0300

    added code to process login

---
+++ login.php
@@ -0,0 +1,3 @@
+<?
+phpif($_POST['password'] =='password' && $_POST['username'] == 'username') {session_start();$_SESSION['username']='username';redirect("list.php");} else {echo 'Login failed';}function redirect($url) {ob_start();header('Location: '.$url);ob_end_flush();die();}
+?>
