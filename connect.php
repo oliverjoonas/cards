7b7dae5cc58e371d6fc0d5bf2cb684345cf45198
Oliver Oliver <sephinele@gmail.com>
2019-09-05 19:39:53 +0300

    Fixed some errors and created connect php file

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+function connect_db() {$servername = "localhost";$username = "cards";$password = "password";$dbname = "cards";
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+die("Connection failed: " . $conn->connect_error);} else {}return $conn;
+}
+?>
+
