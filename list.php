4205378de21a1d2ed033bc1f2c6b60119131acbc
nutopaa5 <neuvonen.joonas@gmail.com>
2019-10-02 09:52:13 +0300

    Added delete function for page

--- list.php
+++ list.php
@@ -18,6 +18,7 @@ echo $row["zip"] . " ";
 echo $row["city"] . " ";
 echo $row["state"] . " ";
 echo $row["country"] . " ";
+echo "<a href=\"delete.php?id=" . $row["id"] . "\">DELETE</a>";
 print("<br>");
 }
 } else {
